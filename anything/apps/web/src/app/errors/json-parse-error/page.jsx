0f6e685dc76594c4a03d6944ea8a5b0c56fdf4ba
Tinfox Consulting <contact@tinfoxconsulting.com>
2026-06-13 19:48:38 +0530

    fixed

--- anything/apps/web/src/app/errors/json-parse-error/page.jsx
+++ anything/apps/web/src/app/errors/json-parse-error/page.jsx
@@ -1,4 +1,13 @@
+// export default function Page() {
+//   const data = JSON.parse('not valid json {{{');
+//   return <div>{data.name}</div>;
+// }
+
 export default function Page() {
-  const data = JSON.parse('not valid json {{{');
-  return <div>{data.name}</div>;
+  try {
+    const data = JSON.parse('not valid json {{{');
+    return <div>{data.name}</div>;
+  } catch (error) {
+    return <div>Something went wrong</div>;
+  }
 }
